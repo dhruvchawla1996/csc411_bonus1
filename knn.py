80432c4e02928ece8b503601cc0b1b8c3cedf2c9
dhruvchawla1996 <dhruvchawla1996@gmail.com>
2018-02-03 16:10:31 -0500

    Forgot to add knn.py in last commit

---
+++ knn.py
@@ -0,0 +1,19 @@
+"""CSC411: Project 1 (bonus)
+   Dhruv Chawla"""
+
+# Imports
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+
+from generate_data import *
+
+n_neighbors = 1
+
+# Dataset and labels
+X = [[0, 0], [1, 1], [5, 4]]
+y = [0, 1, 0]
+
+neigh = KNeighborsClassifier(n_neighbors)
+neigh.fit(X, y)
+
+print neigh.predict(X)
