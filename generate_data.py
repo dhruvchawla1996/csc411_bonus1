29cd65eb17894b3c8908270d2eb8fb4a47e616cb
Chawla Dhruv <chawlad1@teach.cs.toronto.edu>
2018-02-04 15:58:51 -0500

    I hate this

--- generate_data.py
+++ generate_data.py
@@ -15,14 +15,14 @@ def generate_data():
 
     np.random.seed(5)
 
-    for i in range(20):
-        for j in range(20):
+    for i in range(8):
+        for j in range(8):
+            for k in range(5):
+                x_coord = i - 0.5 + np.random.random_sample()
+                y_coord = j - 0.5 + np.random.random_sample()
 
-            x_coord = i - 0.5 + np.random.random_sample()
-            y_coord = j - 0.5 + np.random.random_sample()
-
-            X += [[x_coord, y_coord]]
-            y += [label]
+                X += [[x_coord, y_coord]]
+                y += [label]
 
             label = 1 - label
 
