a5da555f7758977592645822dc81c70f40788359
dhruvchawla1996 <dhruvchawla1996@gmail.com>
2018-02-04 14:38:12 -0500

    Save performance figure

--- knn.py
+++ knn.py
@@ -4,11 +4,16 @@
 # Imports
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 from generate_data import *
 
 # Dataset and labels
 X, y = generate_data()
+
+K = []
+performance = []
 
 # Run kNN with increasing K
 for n in range(1, len(y)):
@@ -20,4 +25,13 @@ for n in range(1, len(y)):
     total = len(prediction)
     correct = sum(1 - np.absolute(y - prediction))
 
-    print(100* correct/float(total))
+    K += [n]
+    performance += [100 * correct/float(total)]
+
+# Plot performace and save image
+plt.plot(K, performace, color="k", linewidth=2, marker="o")
+
+plt.title("Performance on training set with K")
+plt.xlabel("K")
+plt.ylabel("Performance on training set")
+plt.savefig("performace.jpg")
