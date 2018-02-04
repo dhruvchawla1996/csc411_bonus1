c45a49aa611fb979aef6b4b029e1c0c42ccf97a9
dhruvchawla1996 <dhruvchawla1996@gmail.com>
2018-02-04 17:03:54 -0500

    Center black square

--- generate_data.py
+++ generate_data.py
@@ -15,8 +15,20 @@ def generate_data():
 
     np.random.seed(5)
 
+    for k in range(100):
+        x_coord = 1 - 0.5 + np.random.random_sample()
+        y_coord = 1 - 0.5 + np.random.random_sample()
+
+        X += [[x_coord, y_coord]]
+        y += [label]
+
+    label = 1 - label
+
     for i in range(3):
         for j in range(3):
+
+            if i == 1 and j == 1: continue
+
             for k in range(100):
                 x_coord = i - 0.5 + np.random.random_sample()
                 y_coord = j - 0.5 + np.random.random_sample()
@@ -24,7 +36,7 @@ def generate_data():
                 X += [[x_coord, y_coord]]
                 y += [label]
 
-            label = 1 - label
+            #label = 1 - label
 
         #label = 1 - label
 
