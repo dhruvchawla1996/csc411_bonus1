754873c00181609346c299b96da386c6ed900abe
dhruvchawla1996 <dhruvchawla1996@gmail.com>
2018-02-04 12:45:27 -0500

    Chess board patter

--- generate_data.py
+++ generate_data.py
@@ -2,20 +2,29 @@
 import numpy as np
 
 def generate_data():
-	X = []
-	y = []
+    X = []
+    y = []
 
-	X.append([0, 0])
-	y.append(0)
+    # X.append([0, 0])
+    # y.append(0)
 
-	label = 1
+    label = 1
 
-	for i in range(1, 3):
+    # for i in range(1, 3):
 
-		for j in range(8):
-			X += [[i * np.cos(j/8 * 2*np.pi), i * np.sin(j/8 * 2*np.pi)]]
-			y += [label]
+    # 	for j in range(8):
+    # 		X += [[i * np.cos(j/8 * 2*np.pi), i * np.sin(j/8 * 2*np.pi)]]
+    # 		y += [label]
 
-		label = 1 - label
+    # 	label = 1 - label
 
-	return X, y
+    for i in range(8):
+        for j in range(8):
+            X += [[i, j]]
+            y += [label]
+
+            label = 1 - label
+
+        label = 1 - label
+
+    return X, y
